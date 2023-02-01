e20d29e5b68cd3220e66c9b60e742a4ec65abc59
xSaeedii <smk1995m@gmai.com>
2023-02-01 14:41:20 +0300

    miniproduct component with style and work on little order table

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,16 +4,9 @@ import Chart from "../../components/chart/Chart";
 import "./Home.scss";
 import PieChart from "../../components/PieChart/PieChart";
 import Box from "../../components/Box/Box";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import InventoryIcon from "@mui/icons-material/Inventory";
-import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
-import WorkIcon from "@mui/icons-material/Work";
-const boxesData = [
-  { stats: 178, title: "Save Products", boxIcon: FavoriteIcon },
-  { stats: 20, title: "Stock Products", boxIcon: InventoryIcon },
-  { stats: 190, title: "Sales Products", boxIcon: ShoppingBagIcon },
-  { stats: 12, title: "Job Application", boxIcon: WorkIcon },
-];
+
+import MiniProduct from "../../components/MiniProduct/MiniProduct";
+import { boxesData, topProducts } from "../../utils/productsData";
 
 const Home: React.FC = () => {
   return (
@@ -22,7 +15,12 @@ const Home: React.FC = () => {
       <div className="content">
         <div className="top">
           {boxesData.map((data) => (
-            <Box stats={data.stats} title={data.title} Icon={data.boxIcon} />
+            <Box
+              stats={data.stats}
+              title={data.title}
+              Icon={data.boxIcon}
+              key={data.title}
+            />
           ))}
         </div>
         <div className="main-chart">
@@ -31,9 +29,27 @@ const Home: React.FC = () => {
         <div className="main-pie">
           <PieChart />
         </div>
-        <div className="bottom">
+        <div className="bottom-order">
           <div>Orders</div>
-          <div>Top selling</div>
+        </div>
+        <div className="bottom-product">
+          <div className="procut-wrapper">
+            <p>Top Product sellers</p>
+            {topProducts.map((item, idx) => (
+              <React.Fragment key={item.title}>
+                <MiniProduct
+                  title={item.title}
+                  rating={item.rating}
+                  productImg={item.productImg}
+                  price={item.productPrice}
+                />
+                {/* remvoe the seperate line from last product */}
+                {idx === topProducts.length - 1 ? null : (
+                  <hr className="line" />
+                )}
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </div>
